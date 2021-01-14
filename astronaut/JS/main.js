450a1e2ee063e2b2aed2cddfe6ce8a360bf46b1b
cecmon <cecmon@gmail.com>
2021-01-14 00:33:47 -0400

    Subiendo proyecto 3 y 4

---
+++ astronaut/JS/main.js
@@ -0,0 +1,39 @@
+/* MENU SHOW AND HIDDEN */
+const navMenu = document.getElementById("nav-menu"),
+    toggleMenu = document.getElementById("nav-toggle"),
+    closeMenu = document.getElementById("nav-close");
+//Show
+
+toggleMenu.addEventListener("click", () => {
+    navMenu.classList.toggle('show');
+});
+
+//Hide
+
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show');
+});
+
+//Remove class (click list items)
+const navLink = document.querySelectorAll('.nav_link');
+function linkAction() {
+    navMenu.classList.remove('show');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*SCROLL SECTIONS TO ACTIVE LINK*/
+const sections = document.querySelectorAll('section[id]');
+window.addEventListener('scroll', scrollActive);
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav_menu a[href*=' + sectionId +']').classList.add('active');
+        } else{
+            document.querySelector('.nav_menu a[href*=' + sectionId +']').classList.remove('active');
+        }
+    })
+};
